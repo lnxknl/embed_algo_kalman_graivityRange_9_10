886cbc55304487c4cc86c02a371a62631d26dc08
lnxknl4 <jjmutong@gmail.com>
2024-09-27 23:10:35 +0800

    init repo

---
+++ src/kalman_example_gravity.h
@@ -0,0 +1,14 @@
+#ifndef KALMAN_EXAMPLE_GRAVITY_H_
+#define KALMAN_EXAMPLE_GRAVITY_H_
+
+/*!
+* \brief Runs the gravity Kalman filter.
+*/
+void kalman_gravity_demo();
+
+/*!
+* \brief Runs the gravity Kalman filter with lambda tuning.
+*/
+void kalman_gravity_demo_lambda();
+
+#endif
